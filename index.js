315f48b812371fd77dff8e8ac1fee2c9065c2e5e
Carly Pecora <carlyannpecora@gmail.com>
2019-03-29 01:23:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
